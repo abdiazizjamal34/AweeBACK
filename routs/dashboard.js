8e88e7ebba20675a9f6279ba68bc1893ff1bcbf6
abdiaziz <abdiaziz.jamal@aastustudent.edu.et>
2025-04-25 22:23:49 +0300

    dashpord sumerayz and litel update

--- routs/dashboard.js
+++ routs/dashboard.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const { getDashboardSummary } = require('../controlls/dashboardController');
+// const { getDashboardSummary } = require('../controlls/dashboardController');
+const Product = require('../model/Product');
 
-router.get('/summary', getDashboardSummary);
+// router.get('/summary', getDashboardSummary);
+router.get('/', async (req , res) => {
+    try {
+        const totalProducts = await Product.countDocuments();
+        const totalStock = await Product.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    totalStock: { $sum: '$stock' }
+                }
+            }
+        ]);
+
+    const totalValue = await Product.aggregate([
+        {
+            $group: {
+                _id: null,
+                totalValue: { $sum: { $multiply: ['$price', '$stock'] } }
+            }
+        }
+    ]);
+
+  res.status(200).json({
+    totalProducts,
+    totalStock: totalStock[0]?.totalStock || 0,
+    totalValue: totalValue[0]?.totalValue || 0
+  })
+    } catch (err) {
+        res.status(500).json({ message: 'Error generating dashboard summary', error: err.message });
+    }
+
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use('/api/auth',authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/customers', customerRoutes);
 app.use('/api/transactions', transactionRoutes);
-app.use('/api/dashboard', dashboardRoutes);
+app.use('/api/dashboard', dashboardRoutes)
+
 
 
 
@@ -28,6 +29,8 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
+    app.listen(5000, "0.0.0.0", () => {
+      console.log("Server running on all interfaces");
+    });
+      })
   .catch((err) => console.error(err));
